8af3eac6abcf3dfdf8add9d4911cf6deaa61435d
salt-dev <salt-dev@admin-salts-MacBook-Pro.local>
2021-03-12 00:10:03 +0100

    partly working pagination

---
+++ recipe-app-frontend/src/Components/Pagination.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const fetchNextPage = (pageNumber, setPageNumber) => setPageNumber(pageNumber + 1);
+const fetchPreviousPage = (pageNumber, setPageNumber) => setPageNumber(pageNumber - 1);
+const Pagination = ({ pageNumber, setPageNumber, lastPageNumber }) => {
+  if (pageNumber !== 1 || pageNumber !== lastPageNumber) {
+    return (
+      <div className="pagination">
+        <button onClick={() => fetchNextPage(pageNumber, setPageNumber)} className="next-btn"><a href="#">Next</a></button>
+        <button onClick={() => fetchPreviousPage(pageNumber, setPageNumber)} className="prev-btn"><a href="#">Previous</a></button>
+      </div>
+    );
+  } else if (pageNumber === 1) {
+    return (
+      <div className="pagination">
+        <button onClick={() => fetchNextPage(pageNumber, setPageNumber)} className="next-btn"><a href="#">Next</a></button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="pagination">
+        <button onClick={() => fetchPreviousPage(pageNumber, setPageNumber)} className="prev-btn"><a href="#">Previous</a></button>
+      </div>
+    );
+  }
+};
+
+export default Pagination;
